0c788f55c04b83dc049004c2e9596e7f7efc31a2
Harish Narayanan <hnarayanan@gmail.com>
2025-12-03 22:08:12 +0000

    Add a solution for 2025-scheme-3-b

--- 2025-scheme/3/solution-b.scm
+++ 2025-scheme/3/solution-b.scm
@@ -9,19 +9,23 @@
 (define (file->battery-banks path)
   (call-with-input-file path
     (lambda (port)
-      (let loop ((battery-banks '()))
-        (let ((battery-bank (read-line port)))
-          (if (eof-object? battery-bank)
-              (reverse battery-banks)
-              (loop (cons battery-bank battery-banks))))))))
+      (define (loop banks)
+        (let ((line (read-line port)))
+          (if (eof-object? line)
+              (reverse banks)
+              (loop (cons line banks)))))
+      (loop '()))))
 
-;; Write some helpers to convert the strings to list of numbers
-;; representing the battery banks
+;; Write some helpers to convert the strings to lists of numbers and
+;; the other way around
 (define (char->digit char)
   (- (char->integer char) (char->integer #\0)))
 
 (define (string->digits string)
   (map char->digit (string->list string)))
+
+(define (digits->string digits)
+  (apply string-append (map number->string digits)))
 
 ;; Find the largest start digit (and its position) of an n-digit
 ;; number given a list of digits
@@ -36,28 +40,23 @@
               (loop rest (1+ pos) max argmax)))))
   (loop digits 0 0 0))
 
-;; Given a list of digits, construct the largest n-digit number that we can
-(define (find-max-n-digit-number digits n)
+;; Given a list of digits, construct the largest 12-digit number that
+;; we can
+(define (find-max-12-digit-number digits)
   (define (loop remaining found-digits m)
     (if (zero? m)
-        found-digits
+        (string->number (digits->string (reverse found-digits)))
         (let* ((result (find-max-digit-and-pos remaining m))
                (digit (car result))
                (pos (cdr result)))
           (loop (drop remaining (1+ pos))
                 (cons digit found-digits)
                 (1- m)))))
-  (loop digits '() n))
+  (loop digits '() 12))
 
-;; ;; Fetch battery bank information from the input file and process them
-;; (define battery-bank-strings (file->battery-banks "input.txt"))
-;; (define battery-banks (map string->digits battery-bank-strings))
-;; (define max-joltages (map find-max-n-digit-number battery-banks))
-;; (display (apply + max-joltages))
-;; (newline)
-
-
-
-;;   ;; (define max-second-pos (cdr max-second-digit-and-pos))
-
-;;   ;; (+ (* 10 max-first) max-second-digit))
+;; Fetch battery bank information from the input file and process them
+(define battery-bank-strings (file->battery-banks "input.txt"))
+(define battery-banks (map string->digits battery-bank-strings))
+(define max-joltages (map find-max-12-digit-number battery-banks))
+(display (apply + max-joltages))
+(newline)
