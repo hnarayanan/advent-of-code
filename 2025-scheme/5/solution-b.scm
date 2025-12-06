c0e8ddd3bec19e014f5f4031150d0324475f5288
Harish Narayanan <hnarayanan@gmail.com>
2025-12-06 19:38:35 +0000

    Add a solution for 2025-scheme-5-b

--- 2025-scheme/5/solution-b.scm
+++ 2025-scheme/5/solution-b.scm
@@ -5,9 +5,7 @@
              (srfi srfi-1)
              (srfi srfi-11))
 
-;; Given a file containing fresh ingredient ID ranges and available
-;; ingredient IDs, get two lists out of them. First a list of range
-;; bounds and then a list of individual IDs.
+;; Given an input file, get a list ranges
 (define (load-input-file path)
   (call-with-input-file path
     (lambda (port)
@@ -44,66 +42,37 @@
             (loop (cdr lines) ranges))))))
   (loop data '()))
 
-;; We now have enough information to check whether a given ingredient
-;; ID belongs to any of the fresh ingredient ID ranges.
-(define (in-range? n low high)
-  (<= low n high))
-
-(define (in-any-range? n ranges)
-  (any (lambda (range) (in-range? n (car range) (cdr range)))
-       ranges))
-
-;; And with this we can be smarter about how we construct the list of
-;; ranges to begin with.
-(define (insert-sorted range ranges)
-  (cond
-   ((null? ranges) (list range))
-   ((< (car range) (car (car ranges)))
-    (cons range ranges))
-   (else
-    (cons (car ranges)
-          (insert-sorted range (cdr ranges))))))
-
-(define (increase-range-upper-bound range ranges)
-  ;; TODO
-  )
-
-(define (decrease-range-lower-bound range ranges)
-  ;; TODO
-  )
-
+;; Given a list of ranges, merge them in a systematic way
 (define (merge-into-ranges range ranges)
   (cond
-   ;; Case 0: No existing ranges
+   ;; There are no existing ranges, just merge the first one in
    ((null? ranges)
     (list range))
-   ;; Case 1: Neither bound overlaps any existing ranges, just insert
-   ;; the new range in the right place
-   ((and (not (in-any-range? (car range) ranges))
-         (not (in-any-range? (cdr range) ranges)))
-    (insert-sorted range ranges))
-   ;; Case 2: Only lower bound overlaps, then we find that range and
-   ;; increase the upper bound
-   ((and (in-any-range? (car range) ranges)
-         (not (in-any-range? (cdr range) ranges)))
-    (increase-range-upper-bound range ranges))
-   ;; Case 3: Only upper bound overlaps, then we find that range and
-   ;; decrease the lower bound
-   ((and (not (in-any-range? (car range) ranges))
-         (in-any-range? (cdr range) ranges))
-    (decrease-range-lower-bound range ranges))
-
-   ;; Case 4: Both overlap — ???
-   ))
+   ;; Current range entirely before new range - keep it, continue
+   ((< (cdr (car ranges)) (car range))
+    (cons (car ranges) (merge-into-ranges range (cdr ranges))))
+   ;; Current range entirely after new range - insert here, done
+   ((> (car (car ranges)) (cdr range))
+    (cons range ranges))
+   ;; Overlap - merge into bigger range, continue with that
+   (else
+    (merge-into-ranges
+     (cons (min (car range) (car (car ranges)))
+           (max (cdr range) (cdr (car ranges))))
+     (cdr ranges)))))
 
 (define (merged-ranges input-ranges)
   (define (loop remaining ranges)
-    (if (null? (cdr remaining))
+    (if (null? remaining)
         ranges
         (loop (cdr remaining) (merge-into-ranges (car remaining) ranges))))
   (loop input-ranges '()))
 
+(define (range-size range)
+  (1+ (- (cdr range) (car range))))
+
 ;; With all these helpers in place, we run the main program.
-(define input-ranges (parse-input-data (load-input-file "example.txt")))
-(display (merged-ranges input-ranges))
+(define input-ranges (parse-input-data (load-input-file "input.txt")))
+(define total-ids (apply + (map range-size (merged-ranges input-ranges))))
+(display total-ids)
 (newline)
