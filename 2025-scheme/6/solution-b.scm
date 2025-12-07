3219b664caf3cb33d178997462460b2c19441e08
Harish Narayanan <hnarayanan@gmail.com>
2025-12-07 17:24:38 +0000

    Start a whole different approach

--- 2025-scheme/6/solution-b.scm
+++ 2025-scheme/6/solution-b.scm
@@ -4,8 +4,7 @@
 (use-modules (ice-9 rdelim)
              (srfi srfi-13))
 
-;; We first load the input data in rows, even though it's semantically
-;; to be understood in columns. Also, it just so happens that the
+;; We first load the input data in rows. It just so happens that the
 ;; operators are on the last line of the input file, which naturally
 ;; shows up in the first row when loaded in a reversed manner. So we
 ;; keep that in first position and reverse the rest.
@@ -15,62 +14,55 @@
       (define (loop lines)
         (let ((line (read-line port)))
           (if (eof-object? line)
-                   (map string-tokenize
-                        (cons (car lines)
-                              (reverse (cdr lines))))
+              (cons
+               (car lines)
+               (reverse (cdr lines)))
               (loop (cons line lines)))))
       (loop '()))))
 
-;; Using the list procedure to process rows in parallel and ends up
-;; transposing a list of lists
-(define (transpose ll)
-  (apply map list ll))
+;; Parsing this input file is a little tricky, so we rely on the
+;; operator row to determine what the natural column delimiters are.
+;; This took me nearly half a day to arrive at.
+(define (find-column-starts l)
+  (define (loop i remaining positions)
+    (if (null? remaining)
+        (reverse positions)
+        (if (char=? (car remaining) #\space)
+            (loop (1+ i) (cdr remaining) positions)
+            (loop (1+ i) (cdr remaining) (cons i positions)))))
+  (loop 0 (string->list l) '()))
 
-;; Then take the string representations of operators and numbers and
-;; make it a set of expressions that can be evaluated
-(define (right-pad s)
-  (cond ((= (string-length s) 1) (string-append s "_" "_" "_"))
-        ((= (string-length s) 2) (string-append s "_" "_"))
-        ((= (string-length s) 3) (string-append s "_"))
-        (else s)))
+(define (parse ll column-starts)
+  (display ll)
+  (newline)
+  (display column-starts)
+  (newline))
+
+;; ;; Using the list procedure to process rows in parallel and ends up
+;; ;; transposing a list of lists
+;; (define (transpose ll)
+;;   (apply map list ll))
 
 (define (string->op s)
   (cond
    ((string=? s "*") *)
    ((string=? s "+") +)))
 
-(define (something l)
-  (cdr l))
+;; ;; (define (eval-expr expr)
+;; ;;   (let ((op (string->op (car expr)))
+;; ;;         (args (map string->number (cdr expr))))
+;; ;;     (apply op args)))
 
-(define (transposed->cephalopod ll)
-  (define (loop remaining numbers)
-    (if (null? remaining)
-        numbers
-        (append (cdr remaining) numbers)))
-  (loop ll '()))
+;; ;; With all these helpers in place, we run the main program.
+(define input (load-input-file "example.txt"))
+(define column-starts (find-column-starts (car input)))
+(define input-numbers (parse (cdr input) column-starts))
+(define operators (string-tokenize (car input)))
 
-(define (numbers->cephalopod-numbers l)
-  (let* ((padded (map string-reverse (map right-pad l)))
-         (transposed (transpose (map string->list padded)))
-         (cephalopod (transposed->cephalopod transposed)))
-;;    (display cephalopod)
-;;    (newline)
-    transposed))
+;; ;; (define input-data (transpose (load-input-file "example.txt")))
+;; ;; (display (apply + (map eval-expr input-data)))
 
-(define (construct-expr expr)
-  (let ((op (car expr))
-        (args (numbers->cephalopod-numbers (cdr expr))))
-    (append (list op) args)))
-
-;; (define (eval-expr expr)
-;;   (let ((op (string->op (car expr)))
-;;         (args (map string->number (cdr expr))))
-;;     (apply op args)))
-
-;; With all these helpers in place, we run the main program.
-(define input-data (transpose (load-input-file "example.txt")))
-;; (display (apply + (map eval-expr input-data)))
-
-(display (reverse (map construct-expr input-data)))
+;; ;; (display (reverse (map construct-expr input-data)))
+(display operators)
 
 (newline)
