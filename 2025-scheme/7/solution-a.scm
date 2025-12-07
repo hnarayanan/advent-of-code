d44ae83d3d9a13c32ea25e965efc31a96249ea98
Harish Narayanan <hnarayanan@gmail.com>
2025-12-07 21:56:33 +0000

    Wrote an elaborate explanation of what I've just learnt and going to do

--- 2025-scheme/7/solution-a.scm
+++ 2025-scheme/7/solution-a.scm
@@ -26,18 +26,26 @@
 (define (splitter? c)
   (char=? c #\^))
 
+;; Up to this puzzle, we have managed to get away with pure recursive
+;; loops with no mutation. Now we've reached a place where that
+;; approach clearly fights what we need to do:
+;;
+;; For each row, we need to look at the previous row and wherever
+;; there is an S or |, we want to propagate | to the next row. If the
+;; position was previously empty in the next row, this is easy, we
+;; just copy over a |. But if it's not, as in it's a ^, we need to
+;; modify the positions before and after to both be |.
 
-;; For each row, we look at the previous row
-;; Wherever these is an S or |, we want to copy | over to our row
-;; If it's a . in our row, we just copy over | in that position
-;; If it's a ^ in our row, we copy over | to before and after that position
-(define (update-position prev curr)
-  (if (beamy? prev)
-      (if (empty? curr)
-          #\|
-          #\?)
-      curr))
+;; There are ways to solve this (multiple passes, where we first
+;; lookup what needs to be done and then do it) without mutation, but
+;; the approach we follow below is easier. It turns out that while
+;; lists are good for walking left to right, they are not good for
+;; arbitrary lookups and changes. This then means we convert them to
+;; vectors, update them as needed, and then send them back as lists.
 
+copy | over to our row If it's a .
+;; in our row, we just copy over | in that position If it's a ^ in our
+;; row, we copy over | to before and after that position
 (define (propagate-one-step prev current)
   (define (loop remaining-prev remaining-current next)
     (if (or (null? remaining-prev)
@@ -50,17 +58,17 @@
 
 (define (propagate-grid manifold)
   (let loop ((prev-row (car manifold))
-             (rows     (cdr manifold))
-             (acc      (list (car manifold))))
+             (rows (cdr manifold))
+             (grid (list (car manifold))))
     (if (null? rows)
-        (reverse acc)
+        (reverse grid)
         (let* ((current-row (car rows))
                (updated-row (propagate-one-step prev-row current-row)))
           (display updated-row)
           (newline)
           (loop current-row
                 (cdr rows)
-                (cons updated-row acc))))))
+                (cons updated-row grid))))))
 
 ;; (define (count-splits manifold)
 ;;   (define (loop remaining updated count)
