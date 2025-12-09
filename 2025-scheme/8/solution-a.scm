d2102e133e7c5be61fab73059a295e7c3be50ccf
Harish Narayanan <hnarayanan@gmail.com>
2025-12-09 19:14:53 +0000

    Write some helpers to hopefully help solve the problem

--- 2025-scheme/8/solution-a.scm
+++ 2025-scheme/8/solution-a.scm
@@ -14,7 +14,6 @@
                (loop (cons (map string->number (string-split line #\,)) lines)))))
       (loop '()))))
 
-
 ;; Define some helper procedures.
 (define (square x)
   (* x x))
@@ -25,15 +24,23 @@
         (dz (- (caddr p1) (caddr p2))))
     (sqrt (+ (square dx) (square dy) (square dz)))))
 
+(define (make-point points)
+  (lambda (i)
+    (list-ref points i)))
 
+(define (make-pair-distance points)
+  (lambda (i j)
+    (let* ((point (make-point points))
+           (d (distance (point i) (point j))))
+      (list i j d))))
 
 ;; With all these helpers in place, we run the main program.
-(let* ((numbers (load-input-file "example.txt")))
-  (display numbers)
+(let* ((points (load-input-file "example.txt"))
+       (point (make-point points))
+       (pair-distance (make-pair-distance points)))
+  (display points)
   (newline)
-  (display (car numbers))
+  (display (point 0))
   (newline)
-  (display (cadr numbers))
-  (newline)
-  (display (distance (car numbers) (cadr numbers)))
+  (display (pair-distance 0 1))
   (newline))
